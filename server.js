d3baf1d8b475b987672ba36dceeada5b1bb8fa49
Ryan Pena <ryanspena@gmail.com>
2021-08-04 05:17:12 -0500

    server

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-// Server info
-
 const PORT = process.env.PORT || 3001;
 const app = express();
-// Read URL / JSON middleware
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -14,7 +11,6 @@ app.use(express.json());
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-// listener for port
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
